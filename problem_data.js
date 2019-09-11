f00ba200e1b9381f4206c8115240f267d9f7ad46
Ronald C <ronaldc@MACBOOKs-MacBook-Pro.local>
2019-09-11 14:29:24 -0700

    ok

--- problem_data.js
+++ problem_data.js
@@ -1,4 +1,4 @@
-function Problem(name,description,examples,solutionSetup,solution,testinputs,functionName) {
+function Problem(name,description,examples,solutionSetup,solution,testinputs,hint) {
     this.name=name
     this.description=description
     this.examples=examples
@@ -6,7 +6,7 @@ function Problem(name,description,examples,solutionSetup,solution,testinputs,fun
     this.solution=solution
     this.submission=solutionSetup
     this.testinputs=testinputs
-    this.functionName = functionName || name // update problem data to get rid of need for ||
+    this.hint=hint
 }
 
 const PROBLEM_DATA = [
@@ -17,16 +17,16 @@ const PROBLEM_DATA = [
       'function myFirstFunction(input) {\n  \n}',
       x => x,
       [[0],[1],[-2],[235],['256'],[true],['hello']],
-      'myFirstFunction'
+      'Try using the return keyword. Your code should be between the two curly brackets. { }'
     ],
     [
       'sumDouble',
       'Given two int values, return their sum. But if the two values are the same, then return double their sum.',
       'sumDouble(1, 2) → 3 <br> sumDouble(3, 2) → 5 <br> sumDouble(2, 2) → 8 <br>',
-      'function add(a, b) {\n  \n}',
+      'function sumDouble(a, b) {\n  \n}',
       (a,b) => a === b ? 4 * a : a + b,
       [[1,1],[0,2],[2,0],[2,2],[0,0],[5,-1],[-3,-3],[13,12],[0.43,-0.43],[-3,3]],
-      'add'
+      'hint coming soon'
     ],
     [
       'fakeMockup',
