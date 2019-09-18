edca50875c24938a9d03d183179adb8dc5217ae9
Ronald C <ronaldc@MACBOOKs-MacBook-Pro.local>
2019-09-18 09:06:39 -0700

    a

--- problem_data.js
+++ problem_data.js
@@ -13,13 +13,22 @@ function Problem(name,description,examples,solutionSetup,solution,testinputs,hin
 const PROBLEM_DATA = [
     [
       'myFirstFunction',
-      "Just return the input.<br> If you don't know how to do that, then you should <a style=\"color:#505;\" href=\"https://www.google.com/search?q=javascript+how+to+return+a+value&oq=javascript+how+to+return+a+value\" target=\"_blank\">research it</a>. Coders should be good at research.",
+      'Return the input.',
       'myFirstFunction(2) → 2 <br> myFirstFunction(-27) → -27 <br>',
-      'function myFirstFunction(input) {\n  \n}',
+      'function myFirstFunction(x) {\n  \n}',
       x => x,
       [[0],[1],[-2],[235],['256'],[true],['hello']],
-      'Try using <span style="color:purple;font-family:monospace;">return</span>. Your code should be between the two curly brackets. { }'
+      'Type <span style="color:purple;font-family:monospace;"> return </span> <span style="font-family:monospace;"> x </span> between the two braces. { }'
     ],
+    // [
+    //   'myFirstFunction',
+    //   "Just return the input.<br> If you don't know how to do that, then you should <a style=\"color:#505;\" href=\"https://www.google.com/search?q=javascript+how+to+return+a+value&oq=javascript+how+to+return+a+value\" target=\"_blank\">research it</a>. If it's too hard, you could always just give up and do something else.",
+    //   'myFirstFunction(2) → 2 <br> myFirstFunction(-27) → -27 <br>',
+    //   'function myFirstFunction(input) {\n  \n}',
+    //   x => x,
+    //   [[0],[1],[-2],[235],['256'],[true],['hello']],
+    //   'It\'s good to be the kind of person who looks around and sees what clues they could find. Don\'t give up. Try using <span style="color:purple;font-family:monospace;">return</span>. Your code should be between the two curly brackets. { }'
+    // ],
     [
       'add',
       'Create a function that takes two inputs and returns their sum.',
@@ -30,6 +39,15 @@ const PROBLEM_DATA = [
       'Can you guess which symbol is used to perform addition?'
     ],
     [
+      'addExcitement',
+      'Add an exclamation mark to the end of the input string, and return it.',
+      'addExcitement("Hello") → "Hello!" <br> addExcitement("I am a string") → "I am a string!"',
+      'function addExcitement(str) {\n  \n}',
+      x => x + '!',
+      [['Wow'],['!!!'],['OMG'],['I\'m not yelling'],['Here is a symbol you should be familiar with: ']],
+      'The same operator that you use for addition can be used for this.'
+    ],
+    [
       'sumDouble',
       'Given two numbers, return their sum. But if the two values are the same, then return double their sum.',
       'sumDouble(1, 2) → 3 <br> sumDouble(3, 2) → 5 <br> sumDouble(2, 2) → 8 <br>',
@@ -37,14 +55,6 @@ const PROBLEM_DATA = [
       (a,b) => a === b ? 4 * a : a + b,
       [[1,1],[0,2],[2,0],[2,2],[0,0],[5,-1],[-3,-3],[13,12],[0.43,-0.43],[-3,3]],
       'If-statements are one of the most common things you will use when programming. <br> Here is an example program you can study:<br> <span style="font-family:monospace;"> function equals2(x) { <br>&nbsp; if (x == 2) return true; <br>&nbsp; return false; <br> } <br> equals2(2) </span> evaluates to true, and <span style="font-family:monospace;"> equals2(3) </span> evaluates to false. <br> If you\'d like to see for yourself, open the developer tools tab on your browser, <br> and paste the code into the console. Then type <span style="font-family:monospace;"> equals2(2) </span> and press enter.'
-    ],
-    [
-      'code',
-      'This website is still under construction.',
-      'example(input) -> exampleOutput',
-      'function code(x) {\n  \n}',
-      x => x,
-      [[1,1],[0,2],[2,0],[2,2],[0,0],[5,-1],[-3,-3],[13,12],[0.43,-0.43],[1,1],[0,2],[2,0],[2,2],[0,0],[5,-1],[-3,-3],[13,12],[0.43,-0.43],[1,1],[0,2],[2,0],[2,2],[0,0],[5,-1],[-3,-3],[13,12],[0.43,-0.43]]
     ],
     [
       'code',
