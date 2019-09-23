86a11d2adfac120fe93d6eacbbe1af652ce2ea0b
unknown <ronaldmcorona@gmail.com>
2019-09-23 00:18:53 -0700

    UI

--- scripts/setup.js
+++ scripts/setup.js
@@ -117,7 +117,7 @@ Set_Up_Editor: {
   // test different editors:
   // let THEMES = 'dracula,clouds_midnight,solarized_light,solarized_dark,xcode,iplastic,chrome,merbivore_soft,kuroir,idle_fingers,gruvbox,eclipse,crimson_editor,dreamweaver,clouds'.split`,`
   // document.onclick = function pick() { const t = THEMES[COUNT[0]++ % THEMES.length];editor.setTheme("ace/theme/" + t);console.log('theme =',t) }
-  editor.renderer.setShowGutter(false);
+  // editor.renderer.setShowGutter(false);
   const options = {
     enableBasicAutocompletion: true, // the editor completes the statement when you hit Ctrl + Space
     enableLiveAutocompletion: true, // the editor completes the statement while you are typing
